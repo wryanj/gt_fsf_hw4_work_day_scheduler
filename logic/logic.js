a6febcd9006dd02beb037b1f31511ff8aec09eb2
wryanj <ryanjohnson9685@gmail.com>
2021-01-17 14:21:56 -0500

    updated pseudo code in js file to get started

---
+++ logic/logic.js
@@ -0,0 +1,17 @@
+// Define Global Variables
+
+// Add Event Handlers
+
+// Define Script Logic
+
+    // When page loads get current date and time and display in jumbotron...
+
+    // Assign color coding to time blocks based on current date and time...
+
+        // Indicate Past Time Blocks
+
+        // Indicate Present Time Blocks
+
+        // Indicate Future Time Blocks
+
+    // Store schedule entries locally upon click of the save button for that time block
